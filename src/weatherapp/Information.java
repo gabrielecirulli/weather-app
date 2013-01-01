fc6ce71e0abdc85c20a0cda925fb837611123116
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-02 00:34:23 +0100

    trivial comment change

--- src/weatherapp/Information.java
+++ src/weatherapp/Information.java
@@ -1,6 +1,6 @@
 package weatherapp;
 
-// TODO: Move information to XML file
+// TODO: Move information to an XML file
 public class Information {
     static final private int baseWOEID = 7153335; // Friuli Venezia Giulia
     static final String longAppID = "upwCiUvV34FxPD9yS2K6kzLUnBdNA6_kBHk7IGuI8UNi6idv13t4Pv87JHHprzXSGh4eBbaBBJEyohVDz2pUAw1J1kPNI5k";
