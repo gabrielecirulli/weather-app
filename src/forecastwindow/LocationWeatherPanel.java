c48af8cb522d8d6c029c5d33fa1e86f36d91c61a
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-03 00:45:20 +0100

    change formatting of location weather panel

--- src/forecastwindow/LocationWeatherPanel.java
+++ src/forecastwindow/LocationWeatherPanel.java
@@ -1,8 +1,10 @@
 package forecastwindow;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.IOException;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -16,50 +18,54 @@ import yahooweather.YahooLocationLoader;
 import yahooweather.YahooWeather;
 
 class LocationWeatherPanel extends JPanel {
-
+    
     JLabel locationName = new JLabel();
     JLabel weatherCondition = new JLabel();
     LocationWeatherInformationPanel informationPanel =
 	    new LocationWeatherInformationPanel();
-
+    
     public LocationWeatherPanel( int baseWOEID ) throws
 	    ParserConfigurationException, SAXException, IOException,
 	    XPathExpressionException {
-
+	
 	this.setBackground( new Color( 240, 240, 240 ) );
-
+	
+	this.locationName.setFont( this.locationName.getFont().deriveFont( 20f ).deriveFont( Font.BOLD ) );
+	
 	this.setLayout( new GridBagLayout() );
-
+	
 	GridBagConstraints constraints = new GridBagConstraints();
-
+	
+	Utility.resetConstraints( constraints );
 	constraints.gridx = 0;
 	constraints.gridy = 0;
 	constraints.ipady = 20;
 	this.add( locationName, constraints );
-
+	
+	Utility.resetConstraints( constraints );
 	constraints.gridy = 1;
-	constraints.ipady = 0;
 	this.add( weatherCondition, constraints );
 	
+	Utility.resetConstraints( constraints );
 	constraints.gridy = 2;
-	constraints.ipady = 20;
-	this.add( informationPanel, constraints);
-
+	constraints.insets = new Insets( 10, 10, 10, 10 );
+	this.add( informationPanel, constraints );
+	
 	showLocationWeather( baseWOEID );
     }
-
+    
     public final void showLocationWeather( int WOEID ) throws
 	    ParserConfigurationException, SAXException, IOException,
 	    XPathExpressionException {
 	System.out.println( "Show location for: " + WOEID );
 	locationName.setText( YahooLocationLoader.fetchLocationName( WOEID ) );
-
+	
 	YahooWeather weather = YahooForecastLoader.fetchForecast( WOEID );
-
+	
 	String weatherImagePath = String.format( "weather/%02d.png", 1 );
 	weatherCondition.setIcon( new ImageIcon( Utility.loadResource(
 		weatherImagePath ) ) );
 	
-	informationPanel.showWeatherInformation(weather);
+	informationPanel.showWeatherInformation( weather );
     }
 }
