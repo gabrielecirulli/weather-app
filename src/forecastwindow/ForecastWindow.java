9c9e74fe087ab5a2e5679873bb9b81df2863dde2
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-03 13:31:27 +0100

    add pressure display

--- src/forecastwindow/ForecastWindow.java
+++ src/forecastwindow/ForecastWindow.java
@@ -21,35 +21,34 @@ import weatherapp.Information;
 import weatherapp.Utility;
 
 public class ForecastWindow extends JFrame implements ActionListener {
-
+    
     private final LocationSelectorPanel locationSelector;
     private final LocationWeatherPanel locationWeather;
     private final LocationLoadingPanel locationLoading;
     private final int baseWOEID = Information.getBaseWOEID();
-    
     private JMenuBar menuBar;
     private JMenu fileMenu;
     private JMenuItem closeItem;
-
+    
     public ForecastWindow() throws ParserConfigurationException, SAXException,
 	    IOException, XPathExpressionException, Exception {
 	super( Information.getAppName() );
 	this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-
+	
 	this.locationSelector = new LocationSelectorPanel( baseWOEID );
-
+	
 	this.locationWeather = new LocationWeatherPanel( baseWOEID );
-
+	
 	this.locationLoading = new LocationLoadingPanel();
-
+	
 	this.setLayout( new GridBagLayout() );
-
+	
 	GridBagConstraints constraints = new GridBagConstraints();
-
+	
 	constraints.fill = GridBagConstraints.HORIZONTAL;
 	constraints.anchor = GridBagConstraints.PAGE_START;
 	this.add( locationSelector, constraints );
-
+	
 	Utility.resetConstraints( constraints );
 	constraints.weightx = 1;
 	constraints.weighty = 1;
@@ -57,7 +56,7 @@ public class ForecastWindow extends JFrame implements ActionListener {
 	constraints.fill = GridBagConstraints.BOTH;
 	constraints.gridheight = GridBagConstraints.REMAINDER;
 	this.add( locationWeather, constraints );
-
+	
 	this.add( locationLoading, constraints ); // Add loading panel with same constraints
 	locationLoading.setVisible( false ); // Hide loading panel
 
@@ -65,46 +64,46 @@ public class ForecastWindow extends JFrame implements ActionListener {
 
 	// Menu bar for OS X only
 	this.setUpMenu();
-
+	
 	this.setLocationRelativeTo( null );
-
+	
 	this.setVisible( true );
     }
-
+    
     public void showLocation( int WOEID ) throws Exception {
 	System.out.println( "Show location" );
 	System.out.println( WOEID );
-
+	
 	if ( WOEID == 0 ) {
 	    throw new Exception( "WOEID == 0" );
 	}
-
+	
 	this.locationWeather.showLocationWeather( WOEID );
 	/*this.locationLoading.setPreferredSize( this.locationWeather.getSize() );
 	 this.locationWeather.setVisible( false );
 	 this.locationLoading.setVisible( true );*/
-
+	
 	this.validate();
 	this.pack();
     }
-
+    
     public void actionPerformed( ActionEvent event ) {
 	if ( event.getSource() == this.closeItem ) {
 	    this.dispatchEvent( new WindowEvent( this, WindowEvent.WINDOW_CLOSING ) );
 	}
     }
-
+    
     private void setUpMenu() {
 	if ( System.getProperty( "os.name" ).equals( "Mac OS X" ) ) {
 	    this.menuBar = new JMenuBar();
 	    this.fileMenu = new JMenu( "File" );
 	    this.menuBar.add( fileMenu );
-
+	    
 	    this.closeItem = new JMenuItem( "Close Window" );
 	    this.closeItem.setAccelerator( KeyStroke.getKeyStroke( KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask() ) );
 	    this.closeItem.addActionListener( this );
 	    this.fileMenu.add( this.closeItem );
-
+	    
 	    this.setJMenuBar( this.menuBar );
 	}
     }
--- src/forecastwindow/LocationWeatherInformationPanel.java
+++ src/forecastwindow/LocationWeatherInformationPanel.java
@@ -7,44 +7,59 @@ import java.awt.Insets;
 import java.text.DecimalFormat;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import weatherapp.Utility;
 import yahooweather.YahooWeather;
 
 class LocationWeatherInformationPanel extends JPanel {
-    
-    JLabel description = new JLabel();
-    JLabel temperature = new JLabel();
+
+    private JLabel description = new JLabel();
+    private JLabel temperature = new JLabel();
     private DecimalFormat temperatureFormat = new DecimalFormat( "#.#°" );
-    JLabel humidity = new JLabel();
+    private JLabel humidity = new JLabel();
     private DecimalFormat humidityFormat = new DecimalFormat( "Humidity: #.#%" );
+    private JLabel pressure = new JLabel();
+    private DecimalFormat pressureFormat = new DecimalFormat( "Pressure: #.## mbar" );
 
     public LocationWeatherInformationPanel() {
 	this.temperature.setFont( this.temperature.getFont().deriveFont( 30f ) ); // Bigger font size
 	this.description.setFont( this.description.getFont().deriveFont( 14f ).deriveFont( Font.BOLD ) ); // Bold
 
+	this.pressure.setHorizontalAlignment( SwingConstants.CENTER );
+
 	this.setLayout( new GridBagLayout() );
 	GridBagConstraints constraints = new GridBagConstraints();
 
 	constraints.gridwidth = 2;
-	constraints.insets = new Insets( 10, 0, 5, 0);
-	this.add(description, constraints);
-	
-	Utility.resetConstraints(constraints);
+	constraints.insets = new Insets( 10, 0, 5, 0 );
+	this.add( description, constraints );
+
+	Utility.resetConstraints( constraints );
 	constraints.gridy = 2;
+	constraints.weightx = 0.5;
 	this.add( temperature, constraints );
 
-	Utility.resetConstraints(constraints);
+	Utility.resetConstraints( constraints );
 	constraints.gridx = 1;
 	constraints.gridy = 2;
+	constraints.weightx = 1;
 	constraints.insets = new Insets( 0, 20, 0, 0 );
 	this.add( humidity, constraints );
 
+	Utility.resetConstraints( constraints );
+	constraints.gridy = 3;
+	constraints.gridwidth = 2;
+	constraints.insets = new Insets( 10, 0, 0, 0 );
+	this.add( pressure, constraints );
     }
 
     void showWeatherInformation( YahooWeather weather ) {
-	description.setText( weather.getDescription());	
+	description.setText( weather.getDescription() );
 	temperature.setText( temperatureFormat.format( weather.
 		getTemperature() ) );
 	humidity.setText( humidityFormat.format( weather.getHumidity() ) );
+	pressure.setText( String.format( "%s, %s", pressureFormat.format( weather.getPressure() ), ( weather.getPressureDirection() ==
+		0 ? "stationary" : ( weather.getPressureDirection() == 1
+		? "rising" : "lowering" ) ) ) );
     }
 }
