78c6fd082a294a96381646fc113adab7d92a1e38
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-03 14:03:35 +0100

    add sunrise and sunset

--- src/forecastwindow/LocationWeatherInformationPanel.java
+++ src/forecastwindow/LocationWeatherInformationPanel.java
@@ -12,7 +12,7 @@ import weatherapp.Utility;
 import yahooweather.YahooWeather;
 
 class LocationWeatherInformationPanel extends JPanel {
-    
+
     private JLabel description = new JLabel();
     private JLabel temperature = new JLabel();
     private DecimalFormat temperatureFormat = new DecimalFormat( "#.#°" );
@@ -23,46 +23,52 @@ class LocationWeatherInformationPanel extends JPanel {
     private JLabel wind = new JLabel();
     private DecimalFormat windDirection = new DecimalFormat( "#.#°" );
     private DecimalFormat windSpeed = new DecimalFormat( "#.# km/h" );
-    
+    private JLabel sunrise = new JLabel();
+    private JLabel sunset = new JLabel();
+
     public LocationWeatherInformationPanel() {
 	this.temperature.setFont( this.temperature.getFont().deriveFont( 30f ) ); // Bigger font size
 	this.description.setFont( this.description.getFont().deriveFont( 14f ).deriveFont( Font.BOLD ) ); // Bold
 
-	this.pressure.setHorizontalAlignment( SwingConstants.CENTER );
-	
 	this.setLayout( new GridBagLayout() );
 	GridBagConstraints constraints = new GridBagConstraints();
-	
+
 	constraints.gridwidth = 2;
 	constraints.insets = new Insets( 10, 0, 5, 0 );
 	this.add( description, constraints );
-	
+
 	Utility.resetConstraints( constraints );
 	constraints.gridy = 2;
-	constraints.weightx = 0.7;
 	constraints.anchor = GridBagConstraints.CENTER;
 	this.add( temperature, constraints );
-	
+
 	Utility.resetConstraints( constraints );
 	constraints.gridx = 1;
 	constraints.gridy = 2;
-	constraints.weightx = 1;
 	constraints.insets = new Insets( 0, 20, 0, 0 );
 	this.add( humidity, constraints );
-	
+
 	Utility.resetConstraints( constraints );
 	constraints.gridy = 3;
 	constraints.gridwidth = 2;
 	constraints.insets = new Insets( 10, 0, 0, 0 );
 	this.add( pressure, constraints );
-	
+
 	Utility.resetConstraints( constraints );
 	constraints.gridy = 4;
 	constraints.gridwidth = 2;
 	constraints.insets = new Insets( 10, 0, 0, 0 );
 	this.add( wind, constraints );
+
+	Utility.resetConstraints( constraints );
+	constraints.gridy = 5;
+	constraints.insets = new Insets( 10, 0, 0, 0 );
+	this.add( sunrise, constraints );
+
+	constraints.gridx = 1;
+	this.add( sunset, constraints );
     }
-    
+
     void showWeatherInformation( YahooWeather weather ) {
 	description.setText( weather.getDescription() );
 	temperature.setText( temperatureFormat.format( weather.
@@ -72,5 +78,7 @@ class LocationWeatherInformationPanel extends JPanel {
 		0 ? "stationary" : ( weather.getPressureDirection() == 1
 		? "increasing" : "decreasing" ) ) ) );
 	wind.setText( String.format( "Wind: %s at %s", windDirection.format( weather.getWindDirection() ), windSpeed.format( weather.getWindSpeed() ) ) );
+	sunrise.setText( String.format( "Sunrise: %s", weather.getSunriseTime() ) );
+	sunset.setText( String.format( "Sunset: %s", weather.getSunsetTime() ) );
     }
 }
